346bd15cbe2d729b7c408feaf0b83336bb9e6b53
Daniel Tang <dt.tangr@gmail.com>
2012-07-02 23:00:40 +1000

    disable interrupts for moar speeds

--- pcsrc/2ndloader.c
+++ pcsrc/2ndloader.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/time.h>
 #include "xfer.h"
 #include "multi.h"
 #include "loader.h"
@@ -23,12 +24,17 @@ int secondStageLoad(unsigned char* rom, unsigned size, gbaHandle *gba) {
     } while (data != 0xfa57b007 && timeout);
     if (data != 0xfa57b007) return -1;
 
+    struct timeval timebegin, timeend;
+    gettimeofday(&timebegin, NULL);
+
     data = size>>2;
     xferGbaInt32Normal(&data, gba);
     for (i=0;i<size;i+=4) {
         data = rom[i] | (rom[i+1]<<8) | (rom[i+2]<<16) | (rom[i+3]<<24);
         xferGbaInt32Normal(&data, gba);
-        fprintf(stderr,"\r2nd stage loader (%02d%%): %08x",(i*100)/size, data);
+        gettimeofday(&timeend, NULL);
+        double elapsed = (timeend.tv_sec + (timeend.tv_usec / 1000000.0))-(timebegin.tv_sec + (timebegin.tv_usec / 1000000.0));
+        fprintf(stderr,"\r2nd stage loader (%02d%%): %08x (%.0fB/s)",(i*100)/size, data, i/elapsed);
     }
 
     if (data == (0x02000000 + i-4)) fprintf(stderr,"\n2nd stage loading successful\n");
--- pcsrc/main.c
+++ pcsrc/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -9,6 +10,9 @@
 
 int main(int argc, char* argv[]) {
     if (argc < 2) return 0;
+    struct timeval timebegin, timeend;
+    gettimeofday(&timebegin, NULL);
+
 
     gbaHandle *gba = initGbaHandle("/dev/tty.usbmodem12341", MODE_NORMAL);
     FILE *rom = fopen(argv[1],"rb");
@@ -31,6 +35,11 @@ int main(int argc, char* argv[]) {
     free(romdata);
     fclose(rom);
     freeGbaHandle(gba);
+
+    gettimeofday(&timeend, NULL);
+    double executiontime = timeend.tv_sec + (timeend.tv_usec / 1000000.0);
+    executiontime -= timebegin.tv_sec + (timebegin.tv_usec / 1000000.0);
+    fprintf(stderr,"\nTotal transfer time: %.2f seconds\n", executiontime);
     return 0;
 
 }
